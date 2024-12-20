399bcf3f597a07bbb061bcddac06496be02050e1
Amontears <amontearx66@gmail.com>
2024-12-20 13:37:03 +0200

    fix

--- postNotes/notes/users/userprofile.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserProfile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', blank=True)
-
-    def __str__(self):
-        return self.user.username
